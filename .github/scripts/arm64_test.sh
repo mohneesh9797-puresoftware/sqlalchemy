de65e559acf92788525b8b95273b1f66a8762917
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-23 19:55:18 +0530

    Update arm64_test.sh

--- .github/scripts/arm64_test.sh
+++ .github/scripts/arm64_test.sh
@@ -1,5 +1,11 @@
 apt-get update 
-apt-get install -y patchelf coreutils curl unzip python3 python3-pip python3.7 libpython3.7-dev git 
-python3.7 -m pip install --upgrade pip 
-python3.7 -m pip install --upgrade tox setuptools
-tox -e github-nocext -q 
+apt-get install -y coreutils python3 python3-pip python3.7 libpython3.7-dev python3.8 libpython3.8
+python_versions=("python3.6" "python3.7" "python3.8")
+for pyversion in "${python_versions[@]}"
+do
+    $pyversion -m pip install --upgrade pip
+    $pyversion -m pip install --upgrade setuptools tox
+    alias python=$pyversion
+	  tox -e github-nocext -q
+	  tox -e github-cext -q
+done
