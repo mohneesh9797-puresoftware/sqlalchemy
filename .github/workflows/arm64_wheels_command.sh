ecad5571a9f7d902d3e7a6e4d83705d948ba1ad5
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-23 18:12:45 +0530

    Update arm64_wheels_command.sh

--- .github/workflows/arm64_wheels_command.sh
+++ .github/workflows/arm64_wheels_command.sh
@@ -1,7 +1,8 @@
 apt-get update && \
 apt-get install -y patchelf coreutils curl unzip python3 python3-pip python3.7 libpython3.7-dev && \
 python3.7 -m pip install --upgrade pip && \
-python3.7 -m pip install --upgrade setuptools wheel auditwheel && \
-python3.7 setup.py bdist_wheel  --plat-name=manylinux2014_aarch64 -d . && 
-auditwheel repair *.whl -w wheelhouse && \
-ls
+python3.7 -m pip install --upgrade setuptools wheel auditwheel cibuildwheel && \
+#python3.7 setup.py bdist_wheel  --plat-name=manylinux2014_aarch64 -d . && 
+#auditwheel repair *.whl -w wheelhouse && \
+python3.7 -m cibuildwheel --output-dir wheelhouse
+ls wheelhouse
