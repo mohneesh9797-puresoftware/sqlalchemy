83a922b1c9ac6904647a9b0dc8ddeecb5ab4811d
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-23 19:31:08 +0530

    Update arm64_create_wheels.sh

--- .github/scripts/arm64_create_wheels.sh
+++ .github/scripts/arm64_create_wheels.sh
@@ -2,7 +2,7 @@ apt-get update
 apt-get install -y coreutils curl python3 python3-dev python3-pip python3.7 libpython3.7-dev python3.8 libpython3.8-dev
 python_versions=["python3.6", "python3.7", "python3.8"]
 for pyversion in python_versions:
-    $pyversion -m pip install --upgrade pip 
-    $pyversion -m pip install --upgrade setuptools wheel auditwheel 
-    $pyversion setup.py bdist_wheel --plat-name=manylinux2014_aarch64 -d wheelhouse 
+    pyversion -m pip install --upgrade pip
+    pyversion -m pip install --upgrade setuptools wheel auditwheel
+    pyversion setup.py bdist_wheel --plat-name=manylinux2014_aarch64 -d wheelhouse
 ls wheelhouse
