8e451cac24447b4174f69df81601e144b5fc9c22
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-24 10:36:53 +0530

    Update arm64_test.sh

--- .github/scripts/arm64_test.sh
+++ .github/scripts/arm64_test.sh
@@ -4,4 +4,4 @@ echo $python
 apt-get install -qq coreutils python3 python3-pip $python
 $python -m pip install --upgrade pip
 $python -m pip install --upgrade setuptools tox
-tox -e $3 -q
+tox -e $2 -q
