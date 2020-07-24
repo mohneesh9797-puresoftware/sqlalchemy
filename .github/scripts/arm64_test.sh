1ffdac9a310f02db71c158591dc4e50d4ce80cf0
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-24 09:53:27 +0530

    Update arm64_test.sh

--- .github/scripts/arm64_test.sh
+++ .github/scripts/arm64_test.sh
@@ -1,4 +1,5 @@
 apt-get update -qq
+echo $1
 apt-get install -qq coreutils python3 python3-pip python3.7 libpython3.7-dev python3.8 libpython3.8
 python_versions=("python3.6" "python3.7" "python3.8")
 for pyversion in "${python_versions[@]}"
