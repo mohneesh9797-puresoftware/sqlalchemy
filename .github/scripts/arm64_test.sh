237a72d2db95f5bcded7ea84d3bdd6f5f0229233
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-24 11:38:39 +0530

    Update arm64_test.sh

--- .github/scripts/arm64_test.sh
+++ .github/scripts/arm64_test.sh
@@ -3,4 +3,4 @@ apt-get install -qq coreutils python3 python3-pip $1 lib$1-dev
 $1 -m pip install --upgrade pip
 $1 -m pip install --upgrade setuptools tox
 alias python=$1
-tox -e $2 -q
+tox -e $2 -- -q --nomemory
