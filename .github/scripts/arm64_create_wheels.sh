e7ab053c2ef90d5ee87b2903393f8b0135728918
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-30 14:42:46 +0530

    Update arm64_create_wheels.sh

--- .github/scripts/arm64_create_wheels.sh
+++ .github/scripts/arm64_create_wheels.sh
@@ -1,5 +1,5 @@
-apt-get update -qq
-apt-get install -qq coreutils curl python3-pip  $1 lib$1-dev
+yum update -qq
+yum install -qq coreutils curl python3-pip  $1 lib$1-dev
 $1 -m pip install --upgrade pip
 $1 -m pip install --upgrade setuptools wheel
 $1 setup.py bdist_wheel --plat-name=manylinux2014_aarch64 -d wheelhouse
