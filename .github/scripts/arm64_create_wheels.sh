2daa897abcd0b26d42c77481449ff42ff58f8a24
mohneesh9797-puresoftware <58032121+mohneesh9797-puresoftware@users.noreply.github.com>
2020-07-30 16:06:46 +0530

    Update arm64_create_wheels.sh

--- .github/scripts/arm64_create_wheels.sh
+++ .github/scripts/arm64_create_wheels.sh
@@ -1,5 +1,5 @@
 yum update -y
-yum install -y coreutils curl
+yum install -y coreutils curl python3-pip
 alias python = /opt/_internal/cpython-$1*/bin/python
 python -m pip install -U pip setuptools wheel
 python setup.py bdist_wheel -d wheelhouse
